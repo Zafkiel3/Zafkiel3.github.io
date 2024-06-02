017a1a124b2dc32a979574276cc177edfbdf86ba
Zafkiel3 <2099179718@qq.com>
2024-06-02 23:04:16 +0800

    Site updated: 2024-06-02 23:04:16

--- js/fomal.js
+++ js/fomal.js
@@ -3173,8 +3173,8 @@ if (localStorage.getItem("blogbg") != undefined) {
   setBg(localStorage.getItem("blogbg"));
 } else {
   document.getElementById("defineBg").innerText = `:root{
-    --default-bg: url(https://t.mwm.moe/pc);
-    --darkmode-bg:url(https://www.zafkielpic.cc/img/260A95359125E43121E3DA01D3B43B5E.png);
+    --default-bg: url(https://tuchuang.voooe.cn/images/2023/12/11/91745378_p0_master1200.jpg);
+    --darkmode-bg:url(https://t.mwm.moe/pc);
     --mobileday-bg: url(https://t.mwm.moe/mp);
     --mobilenight-bg: url(https://t.mwm.moe/mp);
   }`;
